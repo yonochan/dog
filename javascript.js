93ecb534665f04af67a9bdbe37943d1383fee481
yonochan <metismisha@gmail.com>
2025-03-21 21:07:38 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
-let dog = prompt("Сколько лет собаке? 0-0");
-let human = (dog - 2)*4+21;
-alert("Собаке по человеческим меркам целых " + human + " лет!!!!!!!! ^^");
+let para = ["лаки","ягер", "миша", "лакки", "lucky", "laki", "lakki", "misha", "jager", "yaguer", "iager"];
+
+
+let gest = prompt("наши собаки ");
+
+let aper = gest.toLowerCase;
+
+let truenetru = para.includes(gest);
+
+let slic = gest.slice(0,1);
+let slic2 = gest.slice(1,gest.length);
+
+let lover = slic.toUpperCase() + slic2;
+
+if (truenetru) {
+
+
+    alert(lover + " наша собака!");
+} else {
+    alert("мы таких не знаем");
+}
